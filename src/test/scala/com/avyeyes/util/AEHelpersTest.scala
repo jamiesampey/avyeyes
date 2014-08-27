3037152b74811549855b339ba5e3e5c6dd4a8f9a
Jamie Sampey <jamie.sampey@gmail.com>
2014-08-26 18:12:35 -0600

    Add more AEHelper tests

--- src/test/scala/com/avyeyes/util/AEHelpersTest.scala
+++ src/test/scala/com/avyeyes/util/AEHelpersTest.scala
@@ -5,8 +5,8 @@ import net.liftweb.http.LiftRules
 import net.liftweb.http.S
 import java.util.Calendar
 import scala.xml.NodeSeq
-
 import com.avyeyes.util.AEConstants._
+import org.apache.commons.lang3.RandomStringUtils
 
 object Boot {
   def boot() = {
@@ -50,6 +50,7 @@ class AEHelpersTest extends WebSpec2(Boot.boot _) {
         AEHelpers.strToDbl("23") must_== 23
         AEHelpers.strToDbl("23.0") must_== 23
       }
+      
       "Return 0 for unparsable strings" withSFor("/") in {
         AEHelpers.strToDbl("blah") must_== 0
       }
@@ -60,8 +61,53 @@ class AEHelpersTest extends WebSpec2(Boot.boot _) {
         AEHelpers.sizeToStr(3) must_== "3.0"
         AEHelpers.sizeToStr(3.5) must_== "3.5"
       }
+      
       "Return localized 'Unknown' for size 0" withSFor("/") in {
         AEHelpers.sizeToStr(0) must_== S.?("enum.U")
+      }
+    }
+    
+    "Avalanche size Double to String conversion" should {
+      "Work on sizes of different precesions" withSFor("/") in {
+        AEHelpers.sizeToStr(3) must_== "3.0"
+        AEHelpers.sizeToStr(3.5) must_== "3.5"
+      }
+      
+      "Return localized 'Unknown' for size 0" withSFor("/") in {
+        AEHelpers.sizeToStr(0) must_== S.?("enum.U")
+      }
+    }
+    
+    "Human number to String conversion" should {
+      "Work on normal integers" withSFor("/") in {
+        AEHelpers.humanNumberToStr(4) must_== "4"
+      }
+      
+      "Return localized 'Unknown' for -1" withSFor("/") in {
+        AEHelpers.humanNumberToStr(-1) must_== S.?("enum.U")
+      }
+    }
+    
+    "Valid External ID check" should {
+      "Return false for None" withSFor("/") in {
+        AEHelpers.isValidExtId(None) must beFalse
+      }
+      
+      "Return false if ID length is wrong" withSFor("/") in {
+        val shortExtId = RandomStringUtils.random(ExtIdLength-1, ExtIdChars)
+        val longExtId = RandomStringUtils.random(ExtIdLength+1, ExtIdChars)
+        AEHelpers.isValidExtId(Some(shortExtId)) must beFalse
+        AEHelpers.isValidExtId(Some(longExtId)) must beFalse
+      }
+      
+      "Return false if ID contains a bad char" withSFor("/") in {
+        val goodExtId = RandomStringUtils.random(ExtIdLength, ExtIdChars)
+        AEHelpers.isValidExtId(Some(goodExtId.replace(goodExtId.charAt(4), '~'))) must beFalse
+      }
+      
+      "Return true if ID is valid" withSFor("/") in {
+        val goodExtId = RandomStringUtils.random(ExtIdLength, ExtIdChars)
+        AEHelpers.isValidExtId(Some(goodExtId)) must beTrue
       }
     }
 }
