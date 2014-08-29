0edf1419f8c2d3e1f69bc2233e1909fe690249c4
Jamie Sampey <jamie.sampey@gmail.com>
2014-08-29 15:04:47 -0600

    Add use strict directive to javascript

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,46 +1,50 @@
-require.config({
+(function() {
+  'use strict';
+
+  require.config({
 	baseUrl: '/js',
     paths: {
-    	'earthAsyncLoad': 'lib/require/gearth-loader',
-    	'mapsAsyncLoad': 'lib/require/gmaps-loader',
-        'jquery-ui': 'lib/jquery-ui',
-        'jquery-geocomplete': 'lib/jquery.geocomplete.min',
-        'jquery-fileupload': 'lib/jquery.fileupload',
-        'jquery-iframe-transport': 'lib/jquery.iframe-transport',
-        'geojs': 'lib/geo.min',
-        'lightbox': 'lib/lightbox.min'
+      'earthAsyncLoad': 'lib/require/gearth-loader',
+      'mapsAsyncLoad': 'lib/require/gmaps-loader',
+      'jquery-ui': 'lib/jquery-ui',
+      'jquery-geocomplete': 'lib/jquery.geocomplete.min',
+      'jquery-fileupload': 'lib/jquery.fileupload',
+      'jquery-iframe-transport': 'lib/jquery.iframe-transport',
+      'geojs': 'lib/geo.min',
+      'lightbox': 'lib/lightbox.min'
     }
-});
-
-var avyeyes = null;
-var avyeyesInitGate = $.Deferred();
-var earthLoaded = false;
-var mapsLoaded = false;
-
-var GOOGLE_API_KEY = 'AIzaSyAHuPQo0kaoI-rEGr0q57EkOF2UPNpFP28';
-
-function earthLoadCB() {
-  earthLoaded = true;
-  if (mapsLoaded) {
-	avyeyesInitGate.resolve();
+  });
+    
+  var avyeyes = null;
+  var avyeyesInitGate = $.Deferred();
+  var earthLoaded = false;
+  var mapsLoaded = false;
+    
+  var GOOGLE_API_KEY = 'AIzaSyAHuPQo0kaoI-rEGr0q57EkOF2UPNpFP28';
+    
+  function earthLoadCB() {
+    earthLoaded = true;
+    if (mapsLoaded) {
+      avyeyesInitGate.resolve();
+    }
   }
-}
-
-function mapsLoadCB() {
-  mapsLoaded = true;
-  if (earthLoaded) {
-    avyeyesInitGate.resolve();
+    
+  function mapsLoadCB() {
+    mapsLoaded = true;
+    if (earthLoaded) {
+      avyeyesInitGate.resolve();
+    }
   }
-}
-
-//Start the main app logic.
-requirejs(['earthAsyncLoad', 'mapsAsyncLoad', 'ae-view'], 
-		function (earthAsyncLoad, mapsAsyncLoad, AvyEyesView) {
-	earthAsyncLoad(earthLoadCB);
-	mapsAsyncLoad("mapsLoadCB");
-	
-	avyeyesInitGate.done(function() {
-	  avyeyes = new AvyEyesView(google.earth, google.maps);
-	  avyeyes.init();
-	})
-});
+    
+  //Start the main app logic.
+  requirejs(['earthAsyncLoad', 'mapsAsyncLoad', 'ae-view'], 
+  	function (earthAsyncLoad, mapsAsyncLoad, AvyEyesView) {
+      earthAsyncLoad(earthLoadCB);
+      mapsAsyncLoad("mapsLoadCB");
+    
+      avyeyesInitGate.done(function() {
+        avyeyes = new AvyEyesView(google.earth, google.maps);
+    	avyeyes.init();
+      })
+  });
+})();
