9951e4cde9c4e0934b62cc8cf998e87b49039c66
Jamie Sampey <jamie.sampey@gmail.com>
2014-09-06 19:55:15 -0600

    Make browser not supported redirect a stateless dispatch

--- src/main/scala/bootstrap/Boot.scala
+++ src/main/scala/bootstrap/Boot.scala
@@ -25,7 +25,6 @@ class Boot extends Loggable {
   def boot() = {
     logger.info("LIFT BOOT")
     
-    // where to search snippet
     LiftRules.addToPackages("com.avyeyes")
     
     LiftRules.setSiteMap(SiteMap(
@@ -33,20 +32,19 @@ class Boot extends Loggable {
       Menu.i("Browser Not Supported") / "whawha"
     ))
 
-    LiftRules.dispatch.prepend {
-      case Req(path, _, _) if (path != List("whawha") && !browserSupported(S.request)) => 
+    // external ID URL extraction
+    LiftRules.statelessRewrite.prepend {
+      case RewriteRequest(ParsePath(extId :: Nil, "", _, false), GetRequest, _) => 
+        RewriteResponse(ParsePath("index" :: Nil, "", true, true), Map(ExtIdUrlParam -> extId))
+    }
+    
+    // browser not supported redirect
+    LiftRules.statelessDispatch.prepend {
+      case req @ Req(path, _, _) if (path != List("whawha") && !browserSupported(req)) => 
         () => {Full(RedirectResponse("/whawha.html"))}
     }
     
-    LiftRules.statelessRewrite.prepend {
-        case RewriteRequest(ParsePath(extId :: Nil, "", _, false), GetRequest, _) => 
-            RewriteResponse(ParsePath("index" :: Nil, "", true, true), Map(ExtIdUrlParam -> extId))
-    }
-    
-    LiftRules.maxMimeFileSize = MaxImageSize
-    LiftRules.maxMimeSize = MaxImageSize
-    
-    // setup REST endpoints
+    // setup stateless REST endpoints
     LiftRules.statelessDispatch.append(ImageUpload)
     LiftRules.statelessDispatch.append(ImageServe)
     LiftRules.statelessDispatch.append(ExtIdVendor)
@@ -55,6 +53,8 @@ class Boot extends Loggable {
     // Use HTML5 for rendering
     LiftRules.htmlProperties.default.set((r: Req) => new Html5Properties(r.userAgent))
 
+    LiftRules.maxMimeFileSize = MaxImageSize
+    LiftRules.maxMimeSize = MaxImageSize
     LiftRules.resourceNames = "text" :: "enum" :: "help" :: Nil
   }
   
@@ -63,20 +63,11 @@ class Boot extends Loggable {
   	case "css" :: _ => true
   }
   
-  private def browserSupported(reqBox: Box[Req]): Boolean = reqBox match {
-      case Empty => false
-      case Failure(_ ,_ ,_) => false
-      case Full(request) => { 
-        if (unboxedBrowserVersion(request.chromeVersion) >= ChromeVersion
-            || unboxedBrowserVersion(request.firefoxVersion) >= FirefoxVersion
-            || unboxedBrowserVersion(request.safariVersion) >= SafariVersion
-            || unboxedBrowserVersion(request.ieVersion) >= IeVersion) {
-          true
-        } else {
-          false
-        }
-      }
-  }
+  private def browserSupported(req: Req): Boolean = (
+    unboxedBrowserVersion(req.chromeVersion) >= ChromeVersion
+    || unboxedBrowserVersion(req.firefoxVersion) >= FirefoxVersion
+    || unboxedBrowserVersion(req.safariVersion) >= SafariVersion
+    || unboxedBrowserVersion(req.ieVersion) >= IeVersion)
   
   private def unboxedBrowserVersion(versionBox: Box[Double]): Double = versionBox openOr 0.0
   private def unboxedBrowserVersion(versionBox: Box[Int]): Int = versionBox openOr 0
