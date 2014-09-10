097e470d42cde71b8327941f6337ab35be91a684
Jamie Sampey <jamie.sampey@gmail.com>
2014-09-09 21:16:06 -0600

    Simplify the PersistenceInjector factory method.

--- src/main/scala/com/avyeyes/persist/PersistenceInjector.scala
+++ src/main/scala/com/avyeyes/persist/PersistenceInjector.scala
@@ -3,10 +3,5 @@ package com.avyeyes.persist
 import net.liftweb.http.Factory
 
 object PersistenceInjector extends Factory {
-  val avalancheDao = new FactoryMaker[AvalancheDao](initSquerylAvalancheDao _) {}
-  
-  private def initSquerylAvalancheDao: AvalancheDao = {
-    val squerylDao = new SquerylAvalancheDao
-    squerylDao
-  }
+  val avalancheDao = new FactoryMaker[AvalancheDao](new SquerylAvalancheDao) {}
 }
