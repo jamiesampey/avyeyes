29ccda3d9a671b611c911538c273fec8cba35f0e
Jamie Sampey <jamie.sampey@gmail.com>
2014-09-09 21:03:40 -0600

    More AvyDetails Rest tests. Figured out testing object singletons is tricky with mocks :)

--- src/test/scala/com/avyeyes/rest/AvyDetailsTest.scala
+++ src/test/scala/com/avyeyes/rest/AvyDetailsTest.scala
@@ -1,23 +1,63 @@
 package com.avyeyes.rest
 
+import org.specs2.mutable.Specification
+
+import com.avyeyes.model.Avalanche
+import com.avyeyes.model.enums._
 import com.avyeyes.test._
 
-import net.liftweb.http.S
-import net.liftweb.mocks.MockHttpServletRequest
+import net.liftweb.http._
+import net.liftweb.mockweb.MockWeb
 
-class AvyDetailsTest extends WebSpec2 with MockPersistence with AvalancheGenerator with LiftHelpers {
-  "Avalanche details rest endpoint" should {
-    val a1 = avalancheAtLocation("4jf93dkj", true, 41.6634870900582, -103.875046142935)
-    val mockDetailsRequest = new MockHttpServletRequest("http://avyeyes.com/rest/avydetails/" + a1.extId)
-      
-    "Return avalanche details if avalanche exists" withSFor(mockDetailsRequest) in {
-      mockAvalancheDao.selectViewableAvalanche(a1.extId) returns Some(a1)  
-      mockAvalancheDao.selectAvalancheImageFilenames(a1.extId) returns Nil
-      
-      val req = openLiftReqBox(S.request)
-      val jValue = responseAsJValue(AvyDetails(req)())
+class AvyDetailsTest extends Specification with MockPersistence with AvalancheGenerator with LiftHelpers {
+  // Testing an OBJECT (singleton), so the mockAvalancheDao is inserted ONCE. 
+  // Only one chance to mock all methods.
 
-      extractJsonFieldValue(jValue, "extId") must_== a1.extId
+  val extId1 = "4jf93dkj"
+  val a1 = avalancheAtLocation(extId1, true, 41.6634870900582, -103.875046142935)
+  mockAvalancheDao.selectViewableAvalanche(extId1) returns Some(a1)  
+  mockAvalancheDao.selectAvalancheImageFilenames(extId1) returns Nil
+      
+  val badExtId = "blah"
+  val noAvalanche: Option[Avalanche] = None
+  mockAvalancheDao.selectViewableAvalanche(badExtId) returns noAvalanche
+      
+  "Valid avalanche details REST request" should {
+    "Return avalanche details" in {
+      MockWeb.testS("http://avyeyes.com/rest/avydetails/" + extId1) {
+        val req = openLiftReqBox(S.request)
+        val resp = openLiftRespBox(AvyDetails(req)())
+          
+        resp must beAnInstanceOf[JsonResponse]
+        extractJsonStringField(resp, "extId") must_== extId1
+      }
+    }
+    
+    "Return reader-friendly labels for enum code fields" in {
+      MockWeb.testS("http://avyeyes.com/rest/avydetails/" + extId1) {
+        val req = openLiftReqBox(S.request)
+        val resp = openLiftRespBox(AvyDetails(req)())
+        
+        extractJsonStringField(resp, "submitterExp") must_== ExperienceLevel.getEnumLabel(a1.submitterExp)
+        extractJsonStringField(resp, "sky") must_== Sky.getEnumLabel(a1.sky)
+        extractJsonStringField(resp, "precip") must_== Precip.getEnumLabel(a1.precip)
+        extractJsonStringField(resp, "aspect") must_== Aspect.getEnumLabel(a1.aspect)
+        extractJsonStringField(resp, "avyType") must_== AvalancheType.getEnumLabel(a1.avyType)
+        extractJsonStringField(resp, "trigger") must_== AvalancheTrigger.getEnumLabel(a1.trigger)
+        extractJsonStringField(resp, "bedSurface") must_== AvalancheInterface.getEnumLabel(a1.bedSurface)      
+        extractJsonStringField(resp, "modeOfTravel") must_== ModeOfTravel.getEnumLabel(a1.modeOfTravel)
+      }
+    }
+  }
+  
+  "Invalid avalanche details REST request" should {
+    "Return NotFoundResponse" in {
+      MockWeb.testS("http://avyeyes.com/rest/avydetails/" + badExtId) {
+        val req = openLiftReqBox(S.request)
+        val resp = openLiftRespBox(AvyDetails(req)())
+      
+        resp must beAnInstanceOf[NotFoundResponse]
+      }
     }
   }
 }
--- src/test/scala/com/avyeyes/test/LiftHelpers.scala
+++ src/test/scala/com/avyeyes/test/LiftHelpers.scala
@@ -4,6 +4,7 @@ import net.liftweb.common._
 import net.liftweb.http._
 import net.liftweb.json._
 import net.liftweb.json.JsonAST._
+import java.util.Date
 
 trait LiftHelpers {
   implicit val formats = DefaultFormats
@@ -14,16 +15,16 @@ trait LiftHelpers {
     case Full(req) => req
   }
   
-  def openLiftResponseBox(box: Box[LiftResponse]): LiftResponse = box match {
+  def openLiftRespBox(box: Box[LiftResponse]): LiftResponse = box match {
     case Empty => null
     case Failure(_,_,_) => null
     case Full(resp) => resp
   }
   
-  def responseAsJValue(box: Box[LiftResponse]): JValue = {
-    val response = openLiftResponseBox(box)
+  def jsonResponseAsJValue(response: LiftResponse): JValue = {
     JsonParser.parse(response.asInstanceOf[JsonResponse].json.toJsCmd)
   }
   
-  def extractJsonFieldValue(json: JValue, field: String) = (json \\ field).extract[String]
+  def extractJsonStringField(resp: LiftResponse, field: String):String = (jsonResponseAsJValue(resp) \\ field).extract[String]
+  def extractJsonDoubleField(resp: LiftResponse, field: String): Double = (jsonResponseAsJValue(resp) \\ field).extract[Double]
 }
