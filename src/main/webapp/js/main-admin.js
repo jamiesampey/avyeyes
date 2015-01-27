a9b1f3bb83ca718969e7a335440752c91e7c1855
Jamie Sampey <jamie.sampey@gmail.com>
2015-01-26 18:31:53 -0700

    Datatable auto resizing. Pretty good but not perfect

--- src/main/webapp/js/main-admin.js
+++ src/main/webapp/js/main-admin.js
@@ -7,28 +7,42 @@ require.config({
 //Start the main app logic.
 requirejs(['lib/jquery.dataTables'], function() {
   if (adminLoggedIn()) {
-    var tableOptions = {
-      "columnDefs": [
-        { "name": "createTime", "targets": 0 },
-        { "name": "updateTime", "targets": 1 },
-        { "name": "extId", "targets": 2 },
-        { "name": "viewable", "targets": 3 },
-        { "name": "areaName", "targets": 4 },
-        { "name": "submitterEmail", "targets": 5 }
-      ],
-      "pageLength": 25,
-      "scrollY": "675px",
-      "scrollCollapse": true,
-      "serverSide": true,
-      "ajax": {
-        "dataType": 'json',
-        "type": 'GET',
-        "url": '/rest/admintable'
-      }
-    };
+    wireDataTable();
+  }
+});
 
-  var dataTableApi = $('#avyAdminDataTable').DataTable(tableOptions);
+function adminLoggedIn() {
+  var adminEmailSpan = $('#avyAdminLoggedInEmail');
+  return adminEmailSpan.length > 0 && adminEmailSpan.text().length > 0;
+}
+
+function wireDataTable() {
+  var dataTableObj = $('#avyAdminDataTable');
   var dataTableAjaxFilterTimeout;
+  var calcDataTableHeight = function() {
+    return $(window).height() - 190;
+  };
+
+  var dataTableApi = dataTableObj.DataTable({
+    "columnDefs": [
+       { "name": "createTime", "targets": 0 },
+       { "name": "updateTime", "targets": 1 },
+       { "name": "extId", "targets": 2 },
+       { "name": "viewable", "targets": 3 },
+       { "name": "areaName", "targets": 4 },
+       { "name": "submitterEmail", "targets": 5 }
+     ],
+     "pageLength": 25,
+     "scrollY": calcDataTableHeight(),
+     "scrollCollapse": true,
+     "serverSide": true,
+     "ajax": {
+       "dataType": 'json',
+       "type": 'GET',
+       "url": '/rest/admintable'
+      }
+    }
+  );
 
   $('.dataTables_filter input').unbind().bind('input', function(e) {
     clearTimeout(dataTableAjaxFilterTimeout);
@@ -42,10 +56,8 @@ requirejs(['lib/jquery.dataTables'], function() {
     return;
   });
 
-  }
-});
-
-function adminLoggedIn() {
-  var adminEmailSpan = $('#avyAdminLoggedInEmail');
-  return adminEmailSpan.length > 0 && adminEmailSpan.text().length > 0;
+  $(window).resize(function() {
+    dataTableObj.dataTable().fnSettings().oScroll.sY = calcDataTableHeight();
+    dataTableObj.dataTable().fnDraw();
+  });
 }
