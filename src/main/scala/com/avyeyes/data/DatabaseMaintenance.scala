6f670c69cf62d42a9c5a2998953b0e706638a5b2
Jamie Sampey <jamie.sampey@gmail.com>
2015-07-10 18:36:01 -0600

    a little more tweaking

--- src/main/scala/com/avyeyes/data/DatabaseMaintenance.scala
+++ src/main/scala/com/avyeyes/data/DatabaseMaintenance.scala
@@ -1,13 +1,14 @@
 package com.avyeyes.data
 
 import akka.actor._
+import com.avyeyes.data.DatabaseSchema._
+import com.avyeyes.model.Avalanche
 import com.avyeyes.service.AmazonS3ImageService
 import net.liftweb.common.Loggable
 import slick.driver.PostgresDriver.api._
 
-import scala.concurrent.Await
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
 
 object DatabaseMaintenance {
   val run = "run"
@@ -21,19 +22,21 @@ class DatabaseMaintenance extends Actor with Loggable {
     case DatabaseMaintenance.run => {
       logger.info("Refreshing in-memory avalanche cache")
       AllAvalanchesMap.clear
-      AllAvalanchesMap ++= Await.result(db.run(DatabaseSchema.Avalanches.result).map {
-        _.map(a => (a.extId, a.copy(comments = None))) }, Duration.Inf)
+      AllAvalanchesMap ++= Await.result(getAllAvalanches, Duration.Inf).map(a =>
+        (a.extId, a.copy(comments = None)))
       logger.info(s"Refreshed avalanche cache with ${AllAvalanchesMap.size} avalanches")
 
       logger.info("Pruning orphan images")
-      val extIdsForPrune = pruneImages
+      val extIdsForPrune = Await.result(pruneImages, Duration.Inf)
       extIdsForPrune.foreach(s3.deleteAllImages)
       logger.info(s"Pruned orphan images for ${extIdsForPrune.size} unfinished avalanche reports")
     }
     case _ => logger.error("Received unknown message")
   }
 
-  private def pruneImages: Set[String] = {
+  private def getAllAvalanches(): Future[Seq[Avalanche]] = db.run(Avalanches.result)
+
+  private def pruneImages: Future[Set[String]] = db.run {
 
     //    val orphanImageExtIds = from(avalancheImages)(img => where(
     //      img.avyExtId notIn(from(avalanches)(a => select(a.extId)))) select(img.avyExtId)).distinct.toSet
