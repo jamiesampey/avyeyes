415eab5f77b9391bfa7d1de4471a377aafae5911
Jamie Sampey <jamie.sampey@gmail.com>
2015-10-28 21:07:28 -0600

    UI work

--- src/main/scala/com/avyeyes/snippet/Init.scala
+++ src/main/scala/com/avyeyes/snippet/Init.scala
@@ -47,9 +47,9 @@ class Init extends Loggable {
   private def autoCompleteSourcesCmd: JsCmd = {
     JsRaw(s"$$('.avyTypeAutoComplete').autocomplete('option', 'source', ${write(AvalancheType.values)});"
       + s"$$('.avyTriggerAutoComplete').autocomplete('option', 'source', ${write(AvalancheTrigger.values)});"
-      + s"$$('.avyWindSpeedAutoComplete').autocomplete('option', 'source', ${write(WindSpeed.values)});"
       + s"$$('.avyInterfaceAutoComplete').autocomplete('option', 'source', ${write(AvalancheInterface.values)});"
-      + s"$$('.avyAspectAutoComplete').autocomplete('option', 'source', ${write(Direction.values)});"
+      + s"$$('.avyDirectionAutoComplete').autocomplete('option', 'source', ${write(Direction.values)});"
+      + s"$$('.avyWindSpeedAutoComplete').autocomplete('option', 'source', ${write(WindSpeed.values)});"
       + s"$$('.avyModeOfTravelAutoComplete').autocomplete('option', 'source', ${write(ModeOfTravel.values)});"
       + s"$$('.avyExperienceLevelAutoComplete').autocomplete('option', 'source', ${write(ExperienceLevel.values)});")
       .cmd
