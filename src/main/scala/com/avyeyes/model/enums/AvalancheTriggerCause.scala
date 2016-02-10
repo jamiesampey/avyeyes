84f1e031b4b9e10df66eb8939e853941bf639657
Jamie Sampey <jamie.sampey@gmail.com>
2016-02-09 18:26:03 -0700

    keep cause enum codes unique

--- src/main/scala/com/avyeyes/model/enums/AvalancheTriggerCause.scala
+++ src/main/scala/com/avyeyes/model/enums/AvalancheTriggerCause.scala
@@ -6,12 +6,8 @@ object AvalancheTriggerCause extends AutocompleteEnum {
   type AvalancheTriggerCause = Value
 
   val U = Value("AvalancheTriggerCause.U")
-  val Natural_r = Value("AvalancheTriggerCause.Natural.r")
-  val Natural_y = Value("AvalancheTriggerCause.Natural.y")
-  val Explosive_r = Value("AvalancheTriggerCause.Explosive.r")
-  val Explosive_y = Value("AvalancheTriggerCause.Explosive.y")
-  val Human_c = Value("AvalancheTriggerCause.Human.c")
-  val Human_u = Value("AvalancheTriggerCause.Human.u")
-  val Human_r = Value("AvalancheTriggerCause.Human.r")
-  val Human_y = Value("AvalancheTriggerCause.Human.y")
+  val r = Value("AvalancheTriggerCause.r")
+  val y = Value("AvalancheTriggerCause.y")
+  val c = Value("AvalancheTriggerCause.c")
+  val u = Value("AvalancheTriggerCause.u")
 }
