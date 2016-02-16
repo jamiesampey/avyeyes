a936ed6b15e46e326eb408ace0346dd3201d3935
Jamie Sampey <jamie.sampey@gmail.com>
2016-02-15 21:18:37 -0700

    replace U with empty

--- misc/db/schema-changes.sql
+++ misc/db/schema-changes.sql
@@ -4,4 +4,4 @@
 alter table avalanche_image add sort_order integer not null default 0;
 
 -- Release 1.0.5
-alter table avalanche_classification add trigger_modifier text not null default 'U';
+alter table avalanche_classification add trigger_modifier text not null default 'empty';
