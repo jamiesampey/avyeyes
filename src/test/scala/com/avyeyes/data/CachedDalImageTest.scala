ce7012c0c4798ca9487e6f7f5844e0f83d4bc8c4
Jamie Sampey <jamie.sampey@gmail.com>
2016-11-16 21:53:57 -0700

    more tests working

--- src/test/scala/com/avyeyes/data/CachedDalImageTest.scala
+++ src/test/scala/com/avyeyes/data/CachedDalImageTest.scala
@@ -4,7 +4,7 @@ import com.avyeyes.service.UnauthorizedException
 import com.avyeyes.util.FutureOps._
 import com.avyeyes.test.Generators._
 import org.specs2.mutable.Specification
-
+import scala.concurrent.Future
 
 class CachedDalImageTest extends Specification with InMemoryDB {
   sequential
@@ -18,10 +18,8 @@ class CachedDalImageTest extends Specification with InMemoryDB {
     val img2 = avalancheImageForTest.copy(avalanche = nonExistentAvalancheExtId)
   
     "Image insert and select works" >> {
-      mockUserSession.isAdminSession returns true
+      dal.insertAvalanche(testAvalanche).flatMap(_ => dal.insertAvalancheImage(img1)).resolve
 
-      dal.insertAvalanche(testAvalanche)
-      dal.insertAvalancheImage(img1)
       val returnedImage = dal.getAvalancheImage(testAvalanche.extId, img1.filename).resolve.get
       
       returnedImage.avalanche mustEqual testAvalanche.extId
@@ -30,30 +28,18 @@ class CachedDalImageTest extends Specification with InMemoryDB {
       returnedImage.caption mustEqual img1.caption
     }
     
-    "Image without corresponding avalanche is not selected" >> {
-      mockUserSession.isAdminSession returns false
-
-      dal.insertAvalancheImage(img2)
-      dal.getAvalancheImage(nonExistentAvalancheExtId, img2.filename) mustEqual None
-    }
-    
     "Images select by avy extId only works" >> {
-      mockUserSession.isAdminSession returns true
-
-      dal.insertAvalanche(testAvalanche)
-      dal.insertAvalancheImage(img1)
-      dal.insertAvalancheImage(img2)
+      dal.insertAvalanche(testAvalanche).resolve
+      dal.insertAvalancheImage(img1).resolve
+      dal.insertAvalancheImage(img2).resolve
       
       val returnedImages = dal.getAvalancheImages(testAvalanche.extId).resolve
       returnedImages must haveLength(1)
       returnedImages.head mustEqual img1
     }
 
-    "Image caption update works for authorized session" >> {
-      mockUserSession.isAdminSession returns true
-
-      dal.insertAvalanche(testAvalanche)
-      dal.insertAvalancheImage(img1)
+    "Image caption update works" >> {
+      dal.insertAvalanche(testAvalanche).flatMap( _ => dal.insertAvalancheImage(img1)).resolve
 
       val testCaption = "look at this image!"
       dal.updateAvalancheImageCaption(testAvalanche.extId, img1.filename, Some(testCaption))
@@ -62,71 +48,47 @@ class CachedDalImageTest extends Specification with InMemoryDB {
       imageOpt.get.caption mustEqual Some(testCaption)
     }
 
-    "Image caption remove works for authorized session" >> {
-      mockUserSession.isAdminSession returns true
+    "Image caption remove works" >> {
+      dal.insertAvalanche(testAvalanche).flatMap( _ => dal.insertAvalancheImage(img1.copy(caption = Some("some caption text")))).resolve
 
-      dal.insertAvalanche(testAvalanche)
-      dal.insertAvalancheImage(img1.copy(caption = Some("some caption text")))
-
-      dal.updateAvalancheImageCaption(testAvalanche.extId, img1.filename, None)
+      dal.updateAvalancheImageCaption(testAvalanche.extId, img1.filename, None).resolve
       val imageOpt = dal.getAvalancheImage(testAvalanche.extId, img1.filename).resolve
 
       imageOpt.get.caption must beNone
     }
 
-    "Image caption update does not work for unauthorized session" >> {
-      mockUserSession.isAdminSession returns false
-      dal.updateAvalancheImageCaption(testAvalanche.extId, img1.filename, Some("some caption text")) must throwA[UnauthorizedException]
-    }
-
-    "Image order works for authorized session" >> {
-      mockUserSession.isAdminSession returns true
-
+    "Image order works" >> {
       val order0 = avalancheImageForTest.copy(avalanche = testAvalanche.extId, sortOrder = 0)
       val order1 = avalancheImageForTest.copy(avalanche = testAvalanche.extId, sortOrder = 1)
       val order2 = avalancheImageForTest.copy(avalanche = testAvalanche.extId, sortOrder = 2)
 
-      dal.insertAvalancheImage(order2)
-      dal.insertAvalancheImage(order0)
-      dal.insertAvalancheImage(order1)
+      Future.sequence(List(
+        dal.insertAvalancheImage(order2),
+        dal.insertAvalancheImage(order0),
+        dal.insertAvalancheImage(order1)
+      )).resolve
 
       val firstImageList = dal.getAvalancheImages(testAvalanche.extId).resolve.map(_.filename)
-      dal.updateAvalancheImageOrder(testAvalanche.extId, List(order1.filename, order2.filename, order0.filename))
+      dal.updateAvalancheImageOrder(testAvalanche.extId, List(order1.filename, order2.filename, order0.filename)).resolve
       val secondImageList = dal.getAvalancheImages(testAvalanche.extId).resolve.map(_.filename)
 
       firstImageList mustEqual List(order0.filename, order1.filename, order2.filename)
       secondImageList mustEqual List(order1.filename, order2.filename, order0.filename)
     }
 
-    "Image order does not work for unauthorized session" >> {
-      mockUserSession.isAdminSession returns false
-      dal.updateAvalancheImageOrder(testAvalanche.extId, List("a40ee710", "5d4e3a37")) must throwA[UnauthorizedException]
-    }
-
-    "Image delete works for authorized session" >> {
-      mockUserSession.isAdminSession returns true
-
-      dal.insertAvalanche(testAvalanche)
-      dal.insertAvalancheImage(img1)
+    "Image delete works" >> {
+      dal.insertAvalanche(testAvalanche).flatMap( _ => dal.insertAvalancheImage(img1)).resolve
       
       dal.getAvalancheImage(testAvalanche.extId, img1.filename).resolve must beSome
       dal.deleteAvalancheImage(testAvalanche.extId, img1.filename)
       dal.getAvalancheImage(testAvalanche.extId, img1.filename).resolve must beNone
     }
     
-    "Image delete does not work for unauthorized session" >> {
-      mockUserSession.isAdminSession returns false
-      dal.deleteAvalancheImage(testAvalanche.extId, img1.filename) must throwA[UnauthorizedException]
-    }
-
     "Image count works" >> {
-      mockUserSession.isAdminSession returns false
-
-      dal.insertAvalancheImage(img1)
-      dal.insertAvalancheImage(img2)
-      dal.insertAvalancheImage(avalancheImageForTest.copy(avalanche = img1.avalanche))
-      dal.countAvalancheImages(img1.avalanche) mustEqual 2
-      dal.countAvalancheImages(img2.avalanche) mustEqual 1
+      Future.sequence(List(dal.insertAvalancheImage(img1), dal.insertAvalancheImage(img2))).resolve
+      dal.insertAvalancheImage(avalancheImageForTest.copy(avalanche = img1.avalanche)).resolve
+      dal.countAvalancheImages(img1.avalanche).resolve mustEqual 2
+      dal.countAvalancheImages(img2.avalanche).resolve mustEqual 1
     }
 
   }
--- src/test/scala/com/avyeyes/rest/AvyDetailsTest.scala
+++ src/test/scala/com/avyeyes/rest/AvyDetailsTest.scala
@@ -1,33 +1,33 @@
 package com.avyeyes.rest
 
 import com.avyeyes.data.CachedDAL
-import com.avyeyes.model.Avalanche
 import com.avyeyes.model.JsonSerializers.formats
-import com.avyeyes.service.{UserSession, ResourceService, Injectors}
+import com.avyeyes.service.{UserSession, Injectors}
 import com.avyeyes.test.Generators._
 import com.avyeyes.test.LiftHelpers._
 import com.avyeyes.test._
 import com.avyeyes.util.Constants._
 import com.avyeyes.util.FutureOps._
+import net.liftweb.common.{Full, Box}
 import net.liftweb.http._
 import net.liftweb.json.Extraction
 import org.joda.time.DateTime
+import org.mockito.Matchers
 import org.specs2.execute.{Result, AsResult}
 import org.specs2.mock.Mockito
 import org.specs2.specification.AroundExample
 
+import scala.concurrent.Future
+
 class AvyDetailsTest extends WebSpec2 with AroundExample with Mockito {
 
-  val mockResources = mock[ResourceService]
-  val mockUserSession = mock[UserSession]
+  val mockUser = mock[UserSession]
   val mockAvalancheDal = mock[CachedDAL]
 
   def around[T: AsResult](t: => T): Result =
-    Injectors.resources.doWith(mockResources) {
-      Injectors.user.doWith(mockUserSession) {
-        Injectors.dal.doWith(mockAvalancheDal) {
-          AsResult(t)
-        }
+    Injectors.user.doWith(mockUser) {
+      Injectors.dal.doWith(mockAvalancheDal) {
+        AsResult(t)
       }
     }
 
@@ -38,9 +38,11 @@ class AvyDetailsTest extends WebSpec2 with AroundExample with Mockito {
     isolated
 
     "Return read-only avalanche details" withSFor s"http://avyeyes.com/rest/avydetails/${oldAvalanche.extId}" in {
-      mockUserSession.isAdminSession returns false
-      mockAvalancheDal.getAvalancheFromDisk(oldAvalanche.extId).resolve returns Some(oldAvalanche)
-      mockAvalancheDal.getAvalancheImages(oldAvalanche.extId).resolve returns Nil
+      mockUser.isAuthorizedToEditAvalanche(Matchers.eq(oldAvalanche), any[Box[String]]) returns false
+      mockUser.isAuthorizedToViewAvalanche(Matchers.eq(oldAvalanche)) returns true
+
+      mockAvalancheDal.getAvalancheFromDisk(oldAvalanche.extId) returns Future.successful(Some(oldAvalanche))
+      mockAvalancheDal.getAvalancheImages(oldAvalanche.extId) returns Future.successful(Nil)
 
       val avyDetails = new AvyDetails
 
@@ -55,10 +57,10 @@ class AvyDetailsTest extends WebSpec2 with AroundExample with Mockito {
       extractJsonStringOptionField(resp, "submitterEmail") must beNone
     }
 
-    "Return edit details for admin session" withSFor s"http://avyeyes.com/rest/avydetails/${oldAvalanche.extId}" in {
-      mockUserSession.isAdminSession returns true
-      mockAvalancheDal.getAvalancheFromDisk(oldAvalanche.extId).resolve returns Some(oldAvalanche)
-      mockAvalancheDal.getAvalancheImages(oldAvalanche.extId).resolve returns Nil
+    "Return read-write details for authorized user" withSFor s"http://avyeyes.com/rest/avydetails/${oldAvalanche.extId}" in {
+      mockUser.isAuthorizedToEditAvalanche(Matchers.eq(oldAvalanche), any[Box[String]]) returns true
+      mockAvalancheDal.getAvalancheFromDisk(oldAvalanche.extId) returns Future.successful(Some(oldAvalanche))
+      mockAvalancheDal.getAvalancheImages(oldAvalanche.extId) returns Future.successful(Nil)
 
       val avyDetails = new AvyDetails
 
@@ -73,7 +75,7 @@ class AvyDetailsTest extends WebSpec2 with AroundExample with Mockito {
     }
 
     "Return edit details within edit window with valid edit key" withSFor s"http://avyeyes.com/rest/avydetails/${newAvalanche.extId}?edit=${newAvalanche.editKey}" in {
-      mockUserSession.isAdminSession returns false
+      mockUser.isAuthorizedToEditAvalanche(Matchers.eq(newAvalanche), Matchers.eq(Full(newAvalanche.editKey.toString))) returns true
 
       mockAvalancheDal.getAvalancheFromDisk(newAvalanche.extId).resolve returns Some(newAvalanche)
       mockAvalancheDal.getAvalancheImages(newAvalanche.extId).resolve returns Nil
@@ -91,7 +93,9 @@ class AvyDetailsTest extends WebSpec2 with AroundExample with Mockito {
     }
 
     "Return JSON objects for enum (autocomplete) fields" withSFor s"http://avyeyes.com/rest/avydetails/${oldAvalanche.extId}" in {
-      mockUserSession.isAdminSession returns false
+      mockUser.isAuthorizedToEditAvalanche(Matchers.eq(oldAvalanche), any[Box[String]]) returns false
+      mockUser.isAuthorizedToViewAvalanche(Matchers.eq(oldAvalanche)) returns true
+
       mockAvalancheDal.getAvalancheFromDisk(oldAvalanche.extId).resolve returns Some(oldAvalanche)
       mockAvalancheDal.getAvalancheImages(oldAvalanche.extId).resolve returns Nil
 
@@ -114,8 +118,7 @@ class AvyDetailsTest extends WebSpec2 with AroundExample with Mockito {
     val avyDetails = new AvyDetails
 
     val badExtId = "59fke4k0"
-    val noAvalanche: Option[Avalanche] = None
-    mockAvalancheDal.getAvalancheFromDisk(badExtId).resolve returns noAvalanche
+    mockAvalancheDal.getAvalancheFromDisk(badExtId).resolve returns None
 
     "Return NotFoundResponse (404)" withSFor s"http://avyeyes.com/rest/avydetails/$badExtId" in {
       val req = openLiftReqBox(S.request)
