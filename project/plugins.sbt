9bd6aaa57f0ecdaed76561ba70e61b614ce7712f
Jamie Sampey <jamie.sampey@gmail.com>
2017-04-03 19:44:18 -0600

    kicking things off

--- project/plugins.sbt
+++
@@ -1,3 +0,0 @@
-addSbtPlugin("com.earldouglas" % "xsbt-web-plugin" % "2.0.4")
-
-addSbtPlugin("com.joescii" % "sbt-jasmine-plugin" % "1.3.0")
--- src/test/scala/com/avyeyes/test/TemplateReader.scala
+++
@@ -1,8 +0,0 @@
-package com.avyeyes.test
-
-import scala.xml.XML
-
-trait TemplateReader {
-  lazy val IndexHtmlElem = XML.loadFile("src/main/webapp/index.html")
-  lazy val WhaWhaHtmlElem = XML.loadFile("src/main/webapp/whawha.html")
-}
