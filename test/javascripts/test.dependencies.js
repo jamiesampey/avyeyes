9f0ba464450a386b6f02eeb596b6d4bce81945d0
Jamie Sampey <jamie.sampey@gmail.com>
2018-11-12 02:20:21 -0700

    remove old js specs

--- test/javascripts/test.dependencies.js
+++
@@ -1,5 +0,0 @@
-// Require.js + config
-EnvJasmine.loadGlobal(EnvJasmine.libDir + "jquery.js");
-EnvJasmine.loadGlobal(EnvJasmine.libDir + "require.js");
-EnvJasmine.loadGlobal(EnvJasmine.testDir + "require.conf.js");
-
